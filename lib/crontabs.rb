d2ddbb9c5774dccd1c86f00dd2dcbc150b0b69a1
Rich Howard <rich@kongregate.com>
2015-04-04 17:06:35 -0700

    [more] going to pin work around facters framework

--- lib/crontabs.rb
+++ lib/crontabs.rb
@@ -1,3 +1,16 @@
-class Crontabs
+require 'json'
 
+Facter.add('crontabs') do
+  setcode do
+    operating_system = Facter.value(:operatingsystem)
+    case operating_system
+    when 'Darwin'
+      installed_packages = ''
+    when 'redhat'
+      installed_packages = %x('/bin/rpm -qa').split().to_json
+    when 'debian'
+      installed_packages = ''
+    end
+    installed_packages
+  end
 end
