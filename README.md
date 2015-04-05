085a302799e0413dda0490b2218cf1b1cc1e8efd
Rich Howard <rich@kongregate.com>
2015-04-04 17:05:04 -0700

    [more] going to pin work around facters framework

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # hobart
 
 This project is an attempt to 'profile' a system's state for automation purposes
+using the facter 'fact' system from puppet labs
 
 ## TODO
 everything
--- lib/packages.rb
+++ lib/packages.rb
@@ -1,3 +1,15 @@
-class Packages
+require 'json'
 
+Facter.add('installed_packages') do
+  setcode do
+    operating_system = Facter.value(:operatingsystem)
+    case operating_system
+    when 'Darwin'
+
+      packages = ''
+    when 'redhat'
+      installed_packages = %x('/bin/rpm -qa').split().to_json
+    end
+    installed_packages
+  end
 end
